0ae7c2d575810a633952e64d88865a94e6b7efc5
岡優志 <yu.j.0513@gmail.com>
2021-05-15 23:10:11 +0900

    firstCommit

---
+++ SwiftUITrainigPt3/SwiftUITrainigPt3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITrainigPt3
+//
+//  Created by 岡優志 on 2021/05/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITrainigPt3/SwiftUITrainigPt3/SwiftUITrainigPt3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrainigPt3App.swift
+//  SwiftUITrainigPt3
+//
+//  Created by 岡優志 on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrainigPt3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
